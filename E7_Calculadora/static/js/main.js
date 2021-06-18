cf37db843deab4d2b6c195469e14e3d1152a25ac
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-06-18 17:57:52 -0300

    botao funcionando

--- E7_Calculadora/static/js/main.js
+++ E7_Calculadora/static/js/main.js
@@ -1,51 +1,67 @@
 // get all characters 
 const cards = document.querySelectorAll('.hover-selected')
-const buttons = document.querySelectorAll('button-selected')
+const buttons = document.getElementById('button-selected')
+const props = document.querySelectorAll('.card-info')
 
-// selector for each character
-for (const button of buttons) {
-    button.addEventListener('click', markerButton)
-}
+// for (const prop of props) {
+//     prop.addEventListener('click', markerProps)
+// }
 
 for (const card of cards) {
-card.addEventListener("click", markerChar)
+card.addEventListener("click", markerChampion)
 }
+
+buttons.addEventListener('click', markerButton)
 
 // select character
-function markerChar() {
-    const championsName = this.getAttribute('data-name')
-
-        if (!this.classList.contains('selector')) {
-            this.classList.add('selector')
-
-        } else {
-            this.classList.remove('selector')
-        }
-
-        const championsSelected = document.querySelectorAll('.selector')
-        
-        
-        // selecionar apenas dois personagens
-        if (championsSelected.length >=3) {
-            window.alert('só é possível escolher 2 personagens por rodada')
-            this.classList.remove('selector')
-
-            console.log(championsSelected)
-        }
-}
-
-// select button 
-function markerButton() {
-    const buttonName = this.getAttribute('data-button')
-
+function markerChampion() {
+    const champions = this.getAttribute('data-name')
+    
     if (!this.classList.contains('selector')) {
         this.classList.add('selector')
     } else {
         this.classList.remove('selector')
     }
 
-    const buttonSelected = document.querySelectorAll('.selector')
 }
 
+//                  Fracassei violentamente
+
+// function markerProps() {
+//     const championsProps = this.getAttribute('data-info')
+
+//         if (!this.classList.contains('selector-props')) {
+//             this.classList.add('selector-props')
+//             console.log(championsProps)
+//         } else {
+
+//             this.classList.remove('selector-props')
+//             console.log(championsProps)
+//         }
+
+// }
+
+// select button 
+function markerButton() {
+    const buttonName = this.getAttribute('data-button')
+
+    if (!this.classList.contains('selector-button')) {
+        this.classList.add('selector-button')
+    } else {
+        this.classList.remove('selector-button')
+    }
+
+    const championsSelected = document.querySelectorAll('.selector')
+
+    if (championsSelected.length >= 3) {
+        const buttons = document.getElementById('button-selected')
+
+        buttons.innerHTML = `<button id = 'atacar' type="submit">
+        <p>Negado</p>
+        </button>`
+
+    }
+        
+}
 
 
